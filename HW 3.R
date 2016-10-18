e3e73be126e4d2bea408a2242669df6924032f87
sophiahan3 <sophiahan3@gmail.com>
2016-10-17 17:41:13 -0700

    Add files via upload

---
+++ HW 3.R
@@ -0,0 +1,203 @@
+######################################
+# 12.10.2016
+# Multiple Linear Regression (MLR) example
+# BISC 481
+######################################
+
+## Install packages
+# Bioconductor
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+# DNAshapeR
+biocLite("DNAshapeR")
+# Caret
+install.packages("caret")
+
+## Initialization
+library(DNAshapeR)
+library(caret)
+workingPath <- "/Users/sophiahan/Desktop/Master/gcPBM/"
+
+# 1MER+SHAPE FOR MAD
+## Predict DNA shapes
+fn_fasta <- paste0(workingPath, "Mad.txt.fa")
+pred <- getShape(fn_fasta)
+
+## Encode feature vectors
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Mad.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+               method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+
+# 1MER FOR MAD
+## Encode feature vectors
+featureType <- c("1-mer")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Mad.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+
+
+
+# 1MER+SHAPE FOR MYC
+## Initialization
+library(DNAshapeR)
+library(caret)
+workingPath <- "/Users/sophiahan/Desktop/Master/gcPBM/"
+
+## Predict DNA shapes
+fn_fasta <- paste0(workingPath, "Myc.txt.fa")
+pred <- getShape(fn_fasta)
+
+## Encode feature vectors
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Myc.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+
+
+# 1MER FOR MYC
+## Encode feature vectors
+featureType <- c("1-mer")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Myc.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+
+
+
+# 1MER+SHAPE FOR MAX
+## Initialization
+library(DNAshapeR)
+library(caret)
+workingPath <- "/Users/sophiahan/Desktop/Master/gcPBM/"
+
+## Predict DNA shapes
+fn_fasta <- paste0(workingPath, "Max.txt.fa")
+pred <- getShape(fn_fasta)
+
+## Encode feature vectors
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Max.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+
+
+# 1MER FOR MYC
+## Encode feature vectors
+featureType <- c("1-mer")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Max.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
